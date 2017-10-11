b0f6031fc5c731f11a3989ebafb67084f097aa37
AndrewGit <andrewbryantyeung@gmail.com>
2017-10-11 15:57:35 -0400

    first commit

---
+++ Convert to Camel Case/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convert_to_Camel_Case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test1 = "the_stealth_warrior";
+            string test2 = "The-Stealth-Warrior";
+            //string test = "helloworld";
+            //Console.WriteLine((test[0].ToString()).ToUpper());
+            //Console.ReadLine();
+
+            Console.WriteLine(ToCamelCase(test1));
+            Console.WriteLine(ToCamelCase(test2));
+
+            Console.ReadLine();
+
+
+
+        }
+        public static string ToCamelCase(string str)
+        {
+            string title = str;
+            while (title.Contains("-") || title.Contains("_"))
+            {
+                int spaceLocation = 0;
+                if (title.Contains("-"))
+                {
+                    spaceLocation = title.IndexOf("-");
+                }
+
+                if (title.Contains("_"))
+                {
+                    spaceLocation = title.IndexOf("_");
+                }
+
+                title = title.Remove(spaceLocation, 1);
+                title = title.Substring(0, spaceLocation) + ((title[spaceLocation].ToString()).ToUpper() + title.Substring(spaceLocation + 1));
+            }
+
+
+
+            return title;
+        }
+    }
+}
